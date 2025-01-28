cbb3e64c1ae1b2d863cf8541a17aae207dec5320
claudioihwan <147421106+claudioihwan@users.noreply.github.com>
2025-01-28 21:17:38 +0700

    app.py

--- app.py
+++ app.py
@@ -1,50 +1,50 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_from_directory
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import io
-from PIL import Image
+import os
 
 app = Flask(__name__)
+STATIC_FOLDER = 'static'
 
 def wave_func(x, t, speed, frequency, wavelength, amplitude):
     return amplitude * np.sin(2 * np.pi * frequency * (x / wavelength - speed * t))
 
-def create_animation(frequency, wavelength, amplitude, speed):
+def save_static_image(frequency, wavelength, amplitude, speed, filename='static_image.png'):
     num_coils = 20
     x = np.linspace(0, 4 * np.pi, 1000)
+
     fig, ax = plt.subplots()
     ax.set_xlim(0, 4 * np.pi)
     ax.set_ylim(-1, 1)
-    line, = ax.plot([], [], lw=2)
 
-    def init():
-        line.set_data([], [])
-        return line,
+    t = 0
+    y = np.sin(num_coils * x + wave_func(x, t, speed, frequency, wavelength, amplitude))
+    x_disp = x + wave_func(x, t, speed, frequency, wavelength, amplitude)
+    ax.plot(x_disp, y, lw=2)
 
-    def update(frame):
-        t = frame / 20.0
-        y = np.sin(num_coils * x + wave_func(x, t, speed, frequency, wavelength, amplitude))
-        x_disp = x + wave_func(x, t, speed, frequency, wavelength, amplitude)
-        line.set_data(x_disp, y)
-        return line,
+    if not os.path.exists(STATIC_FOLDER):
+        os.makedirs(STATIC_FOLDER)
 
-    ani = animation.FuncAnimation(fig, update, frames=200, init_func=init, interval=50, blit=True)
-    buf = io.BytesIO()
-    ani.save(buf, format='gif', writer='imagemagick')
-    buf.seek(0)
-    return buf
+    fig.savefig(os.path.join(STATIC_FOLDER, filename))
+    plt.close(fig)
+
+    return filename
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    img = None
     if request.method == 'POST':
         frequency = float(request.form['frequency'])
         wavelength = float(request.form['wavelength'])
         amplitude = float(request.form['amplitude'])
         speed = float(request.form['speed'])
-        gif_buf = create_animation(frequency, wavelength, amplitude, speed)
-        return send_file(gif_buf, mimetype='image/gif')
-    return render_template('index.html')
+        img = save_static_image(frequency, wavelength, amplitude, speed)
+    return render_template('index.html', img=img)
+
+@app.route('/static/<path:filename>')
+def static_file(filename):
+    return send_from_directory(STATIC_FOLDER, filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
