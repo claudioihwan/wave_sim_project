d37f68d36946a3c8547e36e42f12eb6fc876a47e
claudioihwan <147421106+claudioihwan@users.noreply.github.com>
2025-02-14 02:11:17 +0700

    Update home.py

--- templates/home.py
+++ templates/home.py
@@ -2,7 +2,7 @@ from templates.layout import layout
 
 def home():
   return layout(f"""
-                  <div class="flex flex-col items-center justify-center w-full md:w-1/2 min-h-screen md:min-h-fit border-2 border-black rounded-lg">
+                  <div class="flex flex-col items-center justify-center w-full md:w-1/2 min-h-screen md:min-h-fit border-2 border-white rounded-lg">
                     <h1 class="w-full text-center text-3xl font-bold mt-5 mb-6">Gelombang Transversal dan Longitudinal</h1>
                     <img src="https://www.flippingphysics.com/uploads/2/1/1/0/21103672/0319-animated-gif-1_6.gif" alt="Deskripsi Gambar" width="350" height="350">
                     <div class="w-full flex flex-col justify-center items-center mb-6">
