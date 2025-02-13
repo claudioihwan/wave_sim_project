b576e43eecd34bb5c3f1ab5370656533b13ae236
claudioihwan <147421106+claudioihwan@users.noreply.github.com>
2025-02-14 01:21:39 +0700

    Update layout.py

--- templates/layout.py
+++ templates/layout.py
@@ -10,7 +10,7 @@ def layout(content = "", script = ""):
         </style>
       </head>
       <body>
-        <div class="w-full min-h-screen flex flex-col items-center justify-center bg-slate-800 text-white md:p-4" style="font-family: 'Poppins', sans-serif;">
+        <div class="w-full min-h-screen flex flex-col items-center justify-center bg-[https://simplehomemadegifts.com/wp-content/uploads/2013/01/BlackChalkboardBackgroundSmall-1.jpg] text-white md:p-4" style="font-family: 'Poppins', sans-serif;">
           {content}
           <a href="/" class="w-full text-center font-bold mt-10">Kembali ke Home</a>
         </div>
