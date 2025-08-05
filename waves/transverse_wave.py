ee9007349707f89e65d4c3c22219f41b1ffd8034
claudioihwan <147421106+claudioihwan@users.noreply.github.com>
2025-08-05 20:33:27 +0700

    Update transverse_wave.py

--- waves/transverse_wave.py
+++ waves/transverse_wave.py
@@ -18,22 +18,20 @@ def transverse_wave(amplitude_1, amplitude_2, frequency_1, frequency_2, waveleng
   
   y_1 = A_1 * A_1 * np.sin(k_1 * x - W_1 * t) + 4
   y_2 = A_2 * A_1 * np.sin(k_2 * x - W_2 * t) - 4
-  y_2 = y_2+2
+  #y_2 = y_2+2
   line_y_1, = ax.plot(x, y_1, 'b-', linewidth=2, label=f'y1')
   line_y_2, = ax.plot(x, y_2, 'r-', linewidth=2, label=f'y2')
-  #ax.set_xlim(0, 4 * np.pi)
+  
   ax.set_xlabel('t')
   ax.set_ylabel('y')
   ax.set_title('Animasi Gelombang Transversal')
-  ax.set_xticks(np.arange(0,14,1))
-  ax.set_xlim(0,12)
-  ax.set_yticks(np.arange(-12,12,1))
-  #custom_labels = [-'12','-11','-10','-9','-8','-7','-6','-5','-4','-3','2','1','0','1', '2', '3', '4','5','6','7','8','9','10','11','12']
-  #ax.set_yticklabels(custom_labels)
-  ax.set_ylim(-12,12)
+  ax.set_xticks(np.arange(0,15,1))
+  ax.set_xlim(0,15)
+  ax.set_yticks(np.arange(-5,5,1))
+  ax.set_ylim(-5,5)
+  
   ax.set_aspect('equal', adjustable='box')
   ax.legend()
-  #plt.tight_layout()
   plt.grid(True, color='k', linestyle='-', linewidth=0.5)
   plt.show()
 
@@ -49,4 +47,5 @@ def transverse_wave(amplitude_1, amplitude_2, frequency_1, frequency_2, waveleng
   html = ani.to_jshtml()
 
   return html
+
 
