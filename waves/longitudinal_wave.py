a293e797de26da3c4d87160cc9da6e13d5cdac53
claudioihwan <147421106+claudioihwan@users.noreply.github.com>
2026-01-25 03:39:44 +0700

    Refactor longitudinal_wave function and animation

--- waves/longitudinal_wave.py
+++ waves/longitudinal_wave.py
@@ -18,7 +18,7 @@ def longitudinal_wave(amplitude_1 = 1, amplitude_2 = 1, frequency_1 = 0.8, frequ
   num_coils_1 = (2 * np.pi / wavelength_1)  # rad/satuan untuk pegas 1
   num_coils_2 = (2 * np.pi / wavelength_2)  # rad/satuan untuk pegas 2
   x0 = np.linspace(0, 10, 80)  # Posisi x sepanjang pegas
-  colors = ["tab:blue" if i % 2 == 0 else "tab:red" for i in range(len(x))]
+  colors = ["tab:blue" if i % 2 == 0 else "tab:red" for i in range(len(x0))]
 
 
 
@@ -85,6 +85,7 @@ def longitudinal_wave(amplitude_1 = 1, amplitude_2 = 1, frequency_1 = 0.8, frequ
       return ani.to_jshtml()
   
   return create_animation()
+
 
 
 
